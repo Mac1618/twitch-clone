f67c577d1d8b8f24a61d0b21f2134f5e2823d5b4
Mac1618 <mac46099@gmail.com>
2024-01-17 23:10:52 +0800

    Routing Concepts: Learning about basic folder structure, proper naming convention, Api routes, page routes and layouts

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,14 @@
+const AuthLayout = ({ 
+  children 
+}: { 
+  children: React.ReactNode 
+}) => {
+	return (
+		<div>
+			<div className="py-3 w-full bg-blue-400 px-5 mb-5"> Nav bar </div>
+			AuthLayout
+		</div>
+	);
+};
+
+export default AuthLayout;
